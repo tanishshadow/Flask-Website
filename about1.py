4b1a78d50b0deb890c4655eb4fccbea0c0bd9152
Tanish Sarmah <70163670+tanishshadow@users.noreply.github.com>
2020-10-30 21:50:09 +0530

    Create about1.py

---
+++ about1.py
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About us </title>
+</head>
+<body>
+    
+    <h1>This is a about us page</h1>
+    <h2>Hi I am {{name}}</h2>
+    
+</body>
+</html>
